1a0312ac5758642af933d08505d2d83cc825cce3
Bruce Lopes <brucelopes98@gmail.com>
2025-03-20 18:31:30 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Alany, te amo muito! Se me ama também, dá uma risadinha!</h1>
+    return <h1>Alany, te amo muito! Se me ama também, dá uma risadinha! 😁</h1>
 }
 
 export default Home;
