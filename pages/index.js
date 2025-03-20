53b162c4b43d623c3cb7de04ef8519d5cdc2e98c
Bruce Lopes <brucelopes98@gmail.com>
2025-03-20 20:18:46 +0000

    Atualiza a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Alany, te amo muito! Se me ama também, dá uma risadinha! 😁</h1>
+    return <h1>Alany, te amo muito! Se me ama também, dá uma risadinha! 😁😊</h1>
 }
 
 export default Home;
