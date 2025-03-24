1da82eb907262821a1efd98350fba26836d1c0aa
Bruce Lopes <brucelopes98@gmail.com>
2025-03-24 10:20:01 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home () {
-    return <h1>Alany, te amo muito! Se me ama também, dá uma risadinha! 😁😊</h1>
+function Home() {
+  return <h1>Alany, te amo muito! Se me ama também, dá uma risadinha! 😁😊</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("Identação errada");
+}
 export default Home;
